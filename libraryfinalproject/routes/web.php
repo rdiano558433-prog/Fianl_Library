881540917b8adffd218f73753c81f518a48b6d0c
Rumar Diano <r.diano.558433@umindanao.edu.ph>
2026-05-04 20:01:59 -0700

    Fix web routes

--- libraryfinalproject/routes/web.php
+++ libraryfinalproject/routes/web.php
@@ -76,6 +76,7 @@ Route::middleware(['auth', 'role:staff'])->prefix('staff')->name('staff.')->grou
 });
 
 // User Routes
+// User Routes
 Route::middleware(['auth', 'role:user'])->prefix('user')->name('user.')->group(function () {
     Route::get('/dashboard', function () {
         $borrowings = \App\Models\Borrowing::with('book')
